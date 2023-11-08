21d3ea6f404d2f887ac23d373e324ffb9651dd64
Antonl1 <sa-fa@mail.ru>
2023-11-08 19:26:26 +0300

    feat: create basic home page components

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+      <header className="header">
+          <img
+            src="https://w7.pngwing.com/pngs/786/126/png-transparent-logo-contracting-photography-logo-symbol.png"
+            alt="logo"/>
+      </header>
+    );
+};
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+      <div className="content">
+          <div>
+              <img src="https://www.fonedog.com/images/photo-compress/image-compressor-image.jpg" alt=""/>
+          </div>
+          <div>
+              ava+ description
+          </div>
+          <div>My posts
+              <div>
+                  new post
+              </div>
+              <div>
+                  <div>post1</div>
+                  <div>post1</div>
+              </div>
+          </div>
+      </div>
+    );
+};
+
+export default Profile;
